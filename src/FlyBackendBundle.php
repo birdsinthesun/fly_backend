30ce2af631403486b0c15dec88793370b5e455a6
birds in the sun <73081731+birdsinthesun@users.noreply.github.com>
2025-08-10 22:50:47 +0200

    Update FlyBackendBundle.php

--- src/FlyBackendBundle.php
+++ src/FlyBackendBundle.php
@@ -2,8 +2,18 @@
 
 namespace Bits\FlyBackendBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
 
-class FlyBackendBundle extends Bundle
+class FlyBackendBundle extends AbstractBundle
 {
+    
+    public function build(ContainerBuilder $container): void
+    {
+        
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../config')); 
+        $loader->load('services.yaml');
+    }
 }
