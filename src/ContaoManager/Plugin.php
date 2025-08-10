b32652f36567a122d0ca04f69fca24e9e8c9c615
birds in the sun <73081731+birdsinthesun@users.noreply.github.com>
2025-08-10 22:44:43 +0200

    Update Plugin.php

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -4,14 +4,17 @@ namespace Bits\FlyBackendBundle\ContaoManager;
 
 use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Bits\FlyBackendBundle\FlyBackendBundle;
 
 class Plugin implements BundlePluginInterface
 {
-    public function getBundles(array $bundles): iterable
+    public function getBundles(ParserInterface $parser)
     {
-        yield BundleConfig::create(FlyBackendBundle::class)
-            ->setLoadAfter([ContaoCoreBundle::class]);
+        return [
+            BundleConfig::create(FlyBackendBundle::class)
+            ->setLoadAfter([ContaoCoreBundle::class])
+            ];
     }
 }
